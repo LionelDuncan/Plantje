bd160487411a0ad846c03e30109f43d9131cf7d3
Unknown <lionel.vo1997@gmail.com>
2018-04-11 16:18:27 +0200

    jk jk this one

--- plantje.ino
+++ plantje.ino
@@ -54,7 +54,7 @@ const char* ssid = "";
 const char* password =  "";
 
 const char* mqttServer = "";
-//const int mqttPort = ;
+const int mqttPort = 1883;
 const char* mqttUser = "";
 const char* mqttPassword = "";
 
