8f2464a32e6d5683b7ffee1205d1910b4663e3a2
Unknown <lionel.vo1997@gmail.com>
2018-04-11 12:13:20 +0200

    last push

--- plantje.ino
+++ plantje.ino
@@ -1,10 +1,11 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Button.h>        //https://github.com/JChristensen/Button
 
 #include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
+//#include <ESP8266mDNS.h>
+//#include <WiFiUdp.h>
+//#include <ArduinoOTA.h>
 
 #include <PubSubClient.h>
 #include <Wire.h>
@@ -22,7 +23,7 @@
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BME280 bme; // I2C
 
-#define OLED_RESET D8
+#define OLED_RESET D5
 Adafruit_SSD1306 display(OLED_RESET);
 
 #define NUMFLAKES 10
@@ -53,7 +54,7 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-unsigned long delayTime;
+const int interval = 4000; // time water in automode
 
 const char* ssid = "test";
 const char* password =  "testtest123";
@@ -63,20 +64,36 @@ const int mqttPort = 16647;
 const char* mqttUser = "esp";
 const char* mqttPassword = "lionel123";
 
+//sensorvalues
+float temp;
+float pres;
+float hum;
+float alt;
+int ldr;
+int mois;
+
 bool automode = 0; //manual
+bool waterplant = false;
 //Sensors Sensors;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 Servo myservo;
 
-Neotimer servotimer = Neotimer(15);
+Neotimer servotimer = Neotimer(4000);
 Neotimer updatetimer = Neotimer(5000);
 Neotimer wifitimer = Neotimer(500);
 Neotimer mqtttimer = Neotimer(2000);
 Neotimer displaytimer = Neotimer(100);
 Neotimer ldrtimer = Neotimer(100);
-Neotimer debounce = Neotimer(100);
+Neotimer startuptimer = Neotimer(2000);
+Neotimer watertimer = Neotimer(4000);
+
+Button flash = Button(D3, true, true, 25);
+
+int counter = 1;
+int lastReset;
+unsigned long lastWatering;
 
 /*class Display {
   private:
@@ -117,17 +134,32 @@ int counter = 1;
 // https://techtutorialsx.com/2017/04/09/esp8266-connecting-to-mqtt-broker/
 // gitkraken test
 
+unsigned long wifidelay = 0;
+unsigned long bmedelay = 0;
+unsigned long clientdelay = 0;
+unsigned long lastReconnectAttempt = 0;
+unsigned long setupdelay = 0;
+
+bool statuss;
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  Serial.begin(9600);
+  Wire.begin(D7, D6);
+
+  statuss = bme.begin();
+  bmedelay = millis();
+
+  if( millis() - bmedelay > 200){
+    if(!statuss){
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      bmedelay = millis();
+    }
+  }
+  
   pinMode(D2, OUTPUT);
   pinMode(A0, INPUT);
-  pinMode(D3, INPUT);
   pinMode(D0, OUTPUT);
-
-  Wire.begin(D1, D5);
   
   // oled display
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
@@ -136,67 +168,21 @@ void setup() {
   // Show image buffer on the display hardware.
   // Since the buffer is intialized with an Adafruit splashscreen
   // internally, this will display the splashscreen.
-  display.display();
-  delay(2000);
-
-  // Clear the buffer.
-  display.clearDisplay();
-
-  // draw a single pixel
-  display.drawPixel(10, 10, WHITE);
-  // Show the display buffer on the hardware.
-  // NOTE: You _must_ call display after making any drawing commands
-  // to make them visible on the display hardware!
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw many lines
-  testdrawline();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
   
-  Serial.println("Connected to the WiFi network");
+  //Serial.println("Connected to the WiFi network");
+  
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  myservo.attach(D8);
+
+  setup_wifi();
 
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
 
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
- 
-    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
- 
-      Serial.println("connected");  
- 
-    } else {
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
-    }
-  }
-
-  Wire.begin(D7, D6);
-
-  bool status;
-  status = bme.begin();
-    
-   if (!status) {
-      Serial.println("Could not find a valid BME280 sensor, check wiring!");
-      while (1);
-  }
-
-  myservo.attach(D2);
-    
-  client.publish("esp/test", "Hello from ESP8266");
-  client.subscribe("esp/servo");
-  client.subscribe("esp/mode");
-  client.subscribe("esp/moisture");
-
-  delayTime = 1000;
-
   //arduino ota configs
-  ArduinoOTA.onStart([]() {
+  /*ArduinoOTA.onStart([]() {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH)
       type = "sketch";
@@ -223,7 +209,46 @@ void setup() {
   ArduinoOTA.begin();
   Serial.println("Ready");
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());*/
+}
+
+void setup_wifi() {
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  wifidelay = millis();
+  if(millis() - wifidelay > 500){
+      while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      setupdelay = millis();
+      //wifidelay = millis();
+      }
+    }
+      else  {
+      Serial.println("");
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      //delay(500);     
+      }
+  
+}
+
+boolean reconnection() {
+    if (client.connect("ESP", mqttUser, mqttPassword, "esp/lastwill", 1, 1, "offline")) {
+      client.publish("esp/lastwill", "online");
+      Serial.println("connected");
+      client.subscribe("esp/servo");
+      client.subscribe("esp/mode");
+      //client.subscribe("esp/moisture");
+      client.subscribe("esp/values");
+      client.publish("esp/mode", "Manual");
+    }
+    return client.connected();
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -241,127 +266,151 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   String servo = "esp/servo";
   String autostring = "esp/mode";
-  String valuestring = "esp/value";
-  char charBuf[50];
+  String valuestring = "esp/values";
 
   if(top == autostring && message == "Auto"){
     automode = 1; // auto
+    waterplant = 0;
+    //display.clearDisplay();
   }
   
   else if (top == autostring && message == "Manual"){
-    //automode = 0; // manual
+    automode = 0; // manual
+    waterplant = 0;
+    //display.clearDisplay();
   }
    
-  if(top == servo && message == "1" && automode == 0){
-    myservo.write(180);
-    return;
-    //if(servotimer.repeat()){
-    //}
+  if(top == servo && message == "0" && automode == 0 ){
+    waterplant = false;
+    myservo.write(120);
+    lastWatering = millis();
   }
 
-  else if(top == servo && message == "0" && automode == 0){
+  else if(top == servo && message == "1" && automode == 0){
     myservo.write(0);
-    return;
-    //if(servotimer.repeat()){
-    //}
+    waterplant = true;
   }
 
   if (top == valuestring && message == "1" && automode == 0){
     printValues();
   }
- 
-  //Serial.println();
-  //Serial.println("-----------------------");
 }
 
-void autoupdate(){
-  if(automode == 1){
-    
-  }
+void displayfirst(){
+    display.setCursor(0,0);
+    display.print("Temperature: " + String(temp) + " *C");
+    display.setCursor(0,20);
+    display.print("Pressure " + String(pres) + "hPA" );
+    display.setCursor(0,40);
+    display.print("Altitude = " + String(alt) + " m");
 }
+
+void displaysecond(){
+    display.setCursor(0,0);
+    display.print("Humidity =  " + String(hum) + "%");
+    display.setCursor(0,20);
+    display.print("LDR: " + String(ldr));
+    display.setCursor(0,40);
+    display.print("Soil Moisture: " + String(mois));
+}
+
+unsigned long last;
 
 void loop() {
-  //display.clearDisplay();
-  //delay(1000);
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("Hello, world!");
-  display.display();
-  delay(2000);
-  display.clearDisplay();
+  /*if(!statuss){
+    bmedelay = millis();
+    if (millis()- bmedelay > 500){
+      statuss = bme.begin();
+    }
+  }*/
   
-  ArduinoOTA.handle();
-  
-  if(automode == 1){
-    digitalWrite(D0, HIGH);
+  if(!client.connected()){
+    Serial.println("not connected");
+    if(millis() - lastReconnectAttempt > 5000){
+      lastReconnectAttempt = millis();
+    
+      if(reconnection()){
+        lastReconnectAttempt = 0;  
+      }
+    }
+  }
+  else{
+    client.loop();   
+  }
+
+  showDisplay();
+
+  flash.read();
+  if(flash.wasPressed() && automode == 0){
+    automode = 1;
+    client.publish("esp/mode", "Auto");
   }
   
+  else if(flash.wasPressed() && automode == 1){
+    automode = 0;
+    client.publish("esp/mode", "Manual");
+  }
+
+  if(automode == 1) {   //auto
+    digitalWrite(D0, HIGH);
+  }
+
   else if(automode == 0){
     digitalWrite(D0, LOW);
   }
+
+  if(!waterplant){
+    if(automode == 1) {   //auto
+      if(updatetimer.repeat()){
+        printValues();
+      }
   
-  int flash = digitalRead(D3);
-  //int led = digitalRead(D0);
-  Serial.println(flash);
-  
-  if(flash == HIGH){
-    //debounce.start();
-    Serial.println("iets");
-    //while(debounce.waiting()){
-    //  Serial.println("waiting");
-    //}
-    //i/f(debounce.done()){
-    // Serial.println("done");*/
-      automode = 1;
-      //debounce.reset();
-    //} 
+      if(mois < 600){
+        myservo.write(0);
+        waterplant = true;
+        last = millis();
+      }    
+    }
   }
-
-  /*else if(automode == 1 && flash == LOW){
-    //debounce.start();
-
-    //if(debounce.done()){
-      automode == 0;
-      //debounce.reset();
-    //} 
-  }*/
   
-  // put your main code here, to run repeatedly:
-  client.loop();
+  //ArduinoOTA.handle();
 
-  if(updatetimer.repeat()){
-      printValues();
+  if(waterplant){
+      if(automode == 1){
+        if(millis() - last > interval){        
+          myservo.write(120);
+          lastWatering = millis();
+          printValues();
+          waterplant = false;
+        }
+    }
   }
 }
 
 void printValues() {
-    float temp = bme.readTemperature();
+    temp = bme.readTemperature();
     char buftemp[10];
     dtostrf(temp, 6, 2, buftemp);
     client.publish("esp/temperature", buftemp);
-    //Serial.println(temp);
+    Serial.println(temp);
     
-    float pres = bme.readPressure() / 100.0F;
+    pres = bme.readPressure() / 100.0F;
     char bufpres[10];
     dtostrf(pres, 6, 2, bufpres);
     client.publish("esp/pressure", bufpres);
-    //Serial.println(pres);
-
-    float alt = bme.readAltitude(SEALEVELPRESSURE_HPA);
+    
+    alt = bme.readAltitude(SEALEVELPRESSURE_HPA);
     char bufalt[10];
     dtostrf(alt, 6, 2, bufalt);
     client.publish("esp/altitude", bufalt);
-    //Serial.println(alt);
 
-    float hum = bme.readHumidity();
+    hum = bme.readHumidity();
     char bufhum[10];
     dtostrf(hum, 6, 2, bufhum);
     client.publish("esp/humidity", bufhum);
-    //Serial.println(hum);
 
     digitalWrite(D2, LOW);
-    int ldr = analogRead(A0);
+    ldr = analogRead(A0);
     char ldrbuf [4];
     itoa(ldr, ldrbuf, 10);
     client.publish("esp/ldr", ldrbuf);
@@ -372,67 +421,55 @@ void printValues() {
     }
     
     if(ldrtimer.done()){
-      int reading = analogRead(A0);
+      mois = analogRead(A0);
       char buf [4];
-      itoa(reading, buf, 10);
+      itoa(mois, buf, 10);
       client.publish("esp/moisture", buf);
     }
-
-    //display.clear();
-    //display.drawString(0, 0, "Temperature: " + String(temp));
-    //display.display();
 }
 
-void testdrawline() {  
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, 0, i, display.height()-1, WHITE);
-    display.display();
-    delay(1);
+void showDisplay(){
+  if(waterplant){
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.print("watering plant");
+      display.display();
   }
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(0, 0, display.width()-1, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, display.height()-1, i, 0, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int16_t i=display.width()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
 
-  display.clearDisplay();
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, WHITE);
-    display.display();
-    delay(1);
+  else if(!waterplant && client.connected()){
+    if (millis() - lastReset < 5000) {
+      display.clearDisplay();
+      displayfirst();
+      display.display();
+    }
+    
+    else if (millis() - lastReset > 5000 && millis() - lastReset < 10000) {
+      display.clearDisplay();
+      displaysecond();
+      display.display();
+    }
+    
+    else if (millis() - lastReset > 10000 && millis() - lastReset < 15000) {
+      display.clearDisplay();
+      display.setCursor(0,0);
+      unsigned long temp = (millis() - lastWatering) / 1000;
+      display.print(String(temp));
+      display.setCursor(0,20);
+      display.print("Seconds"); 
+      display.display();
+    }
+    
+    else if (millis() - lastReset > 15000) {
+      lastReset = millis();
+      display.clearDisplay();
+    }
   }
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE); 
+
+  else if(!waterplant && !client.connected()){
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.print("Trying to bitconnect!");
     display.display();
-    delay(1);
   }
-  delay(250);
 }
 
